19790d4fb57d218879c64727a65e351e0448636e
SoleGomez23 <solegomez1997@gmail.com>
2023-07-09 11:16:27 -0300

    modificaciones

--- MIReApp/MIReApp/settings.py
+++ MIReApp/MIReApp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Libreria',
+    'Modulos',
 ]
 
 MIDDLEWARE = [
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina'
 
 USE_I18N = True
 

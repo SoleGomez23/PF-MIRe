407082d259f02fb4aaa4fde45c09e931f63e8913
SoleGomez23 <solegomez1997@gmail.com>
2023-07-31 20:01:08 -0300

    creacion vista index principio

--- MIRe/metrica/views.py
+++ MIRe/metrica/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Metrica
+from django.http import HttpResponse,  JsonResponse
+from .forms import MetricaForm 
+
 
 # Create your views here.
+
+def index(request):
+    
+    print('hola mundo')
+    return render(request, 'index.html')
+
+
+
+#def create(request):
+  #  form = MetricaForm()
+
+   # return render(request, 'create.html', {'form' : form}) 
+

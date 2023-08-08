3379f8b05b5bf055220bf8ae9e0c35f0feb58a52
edgardo-g <edgardogasp@hotmail.com>
2023-08-08 09:44:19 -0300

    elimino la carpeta MIRe y dejo solo MIRe2

--- MIRe/metrica/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MetricaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'metrica'
--- MIRe/metrica/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Metrica(models.Model):
-    nombre = models.CharField(max_length=50,primary_key=True, blank = False, null= False),
-    perido = models.CharField(max_length = 100),
-    uMedida = models.CharField(max_length = 100),

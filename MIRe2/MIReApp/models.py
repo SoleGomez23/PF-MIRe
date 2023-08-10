9e366212c6dd5c6ed4fd90d022ca827fec675f2e
SoleGomez23 <solegomez1997@gmail.com>
2023-08-10 20:20:32 -0300

    restricciones de titulo de metrica

--- MIRe2/MIReApp/models.py
+++ MIRe2/MIReApp/models.py
@@ -1,10 +1,17 @@
 from django.core.validators import MinValueValidator
 from django.db import models
-
+from django.core.validators import RegexValidator
 # Create your models here.
 class Metrica(models.Model):
     id = models.AutoField(primary_key=True)
-    titulo = models.CharField(max_length=100, verbose_name='Titulo')
+    titulo = models.CharField(max_length=100, 
+                              verbose_name='Titulo',
+                              unique= True,
+                              validators= [
+                                  RegexValidator(regex= r'^[a-zA-Z\s]*$',
+                                                 message="El nombre no puede contener caracteres especiales.",
+                                                 code='invalid_nombre')
+                              ])
     descripcion = models.TextField(verbose_name='Descripcion', null=True) 
     unidad_medida = models.CharField(max_length=100, verbose_name='Unidad de medida')
     valor = models.IntegerField(verbose_name='Valor', validators=[MinValueValidator(0)], default=0)

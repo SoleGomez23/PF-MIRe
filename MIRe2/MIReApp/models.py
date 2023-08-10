af805a18142e93dbb2d76e106df056b41d3986bf
edgardo-g <edgardogasp@hotmail.com>
2023-08-10 10:42:28 -0300

    agrego la restriccion de valores no negativos

--- MIRe2/MIReApp/models.py
+++ MIRe2/MIReApp/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 # Create your models here.
@@ -6,7 +7,7 @@ class Metrica(models.Model):
     titulo = models.CharField(max_length=100, verbose_name='Titulo')
     descripcion = models.TextField(verbose_name='Descripcion', null=True) 
     unidad_medida = models.CharField(max_length=100, verbose_name='Unidad de medida')
-    valor = models.IntegerField(verbose_name='Valor', default=0)
+    valor = models.IntegerField(verbose_name='Valor', validators=[MinValueValidator(0)], default=0)
 
     def __str__(self):
         fila = 'Titulo: ' + self.titulo + ' - ' + 'Descripcion: ' + self.descripcion

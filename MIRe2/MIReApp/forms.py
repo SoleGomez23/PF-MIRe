ca11107184e5ddbfc646f4411384d6fc41c51ce2
SoleGomez23 <solegomez1997@gmail.com>
2023-08-11 15:39:51 -0300

    detalles de edicion de descripcion

--- MIRe2/MIReApp/forms.py
+++ MIRe2/MIReApp/forms.py
@@ -4,5 +4,12 @@ from .models import Metrica
 class MetricaForm(forms.ModelForm):
     class Meta:
         model = Metrica
+        fields = '__all__'
+
+
+class MetricaFormEditar(forms.ModelForm):
+    class Meta:
+        model = Metrica
         fields = ['descripcion']
+        
        
--- MIRe2/MIReApp/views.py
+++ MIRe2/MIReApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Metrica
 from .forms import MetricaForm
+from .forms import MetricaFormEditar
 from django.shortcuts import get_object_or_404, render
 
 def inicio(request):
@@ -26,9 +27,9 @@ def crear_metricas(request):
 
 def editar_metricas(request, id):
     metrica = get_object_or_404(Metrica, id=id)
-    formulario = MetricaForm(request.POST or None, request.FILES or None, instance=metrica)
+    formulario = MetricaFormEditar(request.POST or None, request.FILES or None, instance=metrica)
     if request.method == 'POST':
-        formulario = MetricaForm(request.POST, instance=metrica)
+        formulario = MetricaFormEditar(request.POST, instance=metrica)
         if formulario.is_valid():
             formulario.save()
         return redirect('metricas')

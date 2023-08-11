56eeaff3a1451c0e34cc79ed01718d203611faa9
edgardo-g <edgardogasp@hotmail.com>
2023-08-11 09:41:52 -0300

    agrego la restriccion del formato de unidad de medida

--- MIRe2/MIReApp/models.py
+++ MIRe2/MIReApp/models.py
@@ -1,4 +1,5 @@
 from django.core.validators import MinValueValidator
+from django.core.validators import RegexValidator
 from django.db import models
 
 # Create your models here.
@@ -6,9 +7,18 @@ class Metrica(models.Model):
     id = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=100, verbose_name='Titulo')
     descripcion = models.TextField(verbose_name='Descripcion', null=True) 
-    unidad_medida = models.CharField(max_length=100, verbose_name='Unidad de medida')
-    valor = models.IntegerField(verbose_name='Valor', validators=[MinValueValidator(0)], default=0)
+    unidad_medida = models.CharField(max_length=100, 
+                                        verbose_name='Unidad de Medida',
+                                        unique= True,
+                                        validators= [
+                                            RegexValidator(regex= r'^[a-zA-Z\s]*$',
+                                                            message="La unidad de medida no puede contener caracteres especiales",
+                                                            code='invalid_nombre')
+                                        ])
+    valor = models.IntegerField(verbose_name='Valor', validators=[MinValueValidator(0, message="El valor no puede ser negativo")], default=0)
 
     def __str__(self):
         fila = 'Titulo: ' + self.titulo + ' - ' + 'Descripcion: ' + self.descripcion
         return fila
+
+#unidad_medida = models.CharField(max_length=100, verbose_name='Unidad de medida')

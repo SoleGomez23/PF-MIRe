834bc27449453e930648a86e91fb17b20792fe9d
edgardo-g <edgardogasp@hotmail.com>
2023-08-15 14:20:56 -0300

    le agregue un limite al cuadro de texto de descripcion

--- MIRe2/MIReApp/forms.py
+++ MIRe2/MIReApp/forms.py
@@ -1,15 +1,20 @@
-from django import forms 
+from django import forms
 from .models import Metrica
+from django.forms import Textarea
 
 class MetricaForm(forms.ModelForm):
     class Meta:
         model = Metrica
-        fields = '__all__'
+        fields = '__all__' 
+        widgets = {
+            'descripcion': Textarea(attrs={'cols': 30, 'rows': 1}),
+   }  
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for i in self.fields:
             self.fields[i].widget.attrs.update({'class':'form-control'})
+
 class MetricaFormEditar(forms.ModelForm):
     class Meta:
         model = Metrica

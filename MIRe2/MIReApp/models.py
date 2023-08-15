48e666baa40c9c59e5f573b93904868576435aa5
edgardo-g <edgardogasp@hotmail.com>
2023-08-15 12:02:38 -0300

    agregue el campo frecuencia de medicion

--- MIRe2/MIReApp/models.py
+++ MIRe2/MIReApp/models.py
@@ -1,7 +1,14 @@
 from django.core.validators import MinValueValidator
 from django.core.validators import RegexValidator
 from django.db import models
-from django.core.validators import RegexValidator
+ 
+FRECUENCIAS = (
+    ('Bianual','Bianual'),
+    ('Anual', 'Anual'),
+    ('Semestral','Semestral'),
+    ('Mensual','Mensual')
+)
+ 
 # Create your models here.
 class Metrica(models.Model):
     id = models.AutoField(primary_key=True)
@@ -13,7 +20,7 @@ class Metrica(models.Model):
                                                  message="El nombre no puede contener caracteres especiales.",
                                                  code='invalid_nombre')
                               ])
-    descripcion = models.TextField(verbose_name='Descripcion', null=True) 
+    descripcion = models.TextField(max_length=100, verbose_name='Descripcion', null=True) 
     unidad_medida = models.CharField(max_length=100, 
                                         verbose_name='Unidad de Medida',
                                         unique= True,
@@ -23,9 +30,8 @@ class Metrica(models.Model):
                                                             code='invalid_nombre')
                                         ])
     valor = models.IntegerField(verbose_name='Valor', validators=[MinValueValidator(0, message="El valor no puede ser negativo")], default=0)
+    frecuencia = models.CharField(max_length=10, choices=FRECUENCIAS, default='Anual')
 
     def __str__(self):
         fila = 'Titulo: ' + self.titulo + ' - ' + 'Descripcion: ' + self.descripcion
         return fila
-
-#unidad_medida = models.CharField(max_length=100, verbose_name='Unidad de medida')

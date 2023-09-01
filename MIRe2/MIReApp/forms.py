c2cb0d4b11815496614d7a9687251ab7508cca46
edgardo-g <edgardogasp@hotmail.com>
2023-09-01 10:13:30 -0300

    agrego la restriccion de Ambito y Tipo

--- MIRe2/MIReApp/forms.py
+++ MIRe2/MIReApp/forms.py
@@ -1,6 +1,5 @@
 from django import forms 
-from .models import Metrica
-from .models import Indicador
+from .models import Metrica, Indicador, Tipo
 
 class MetricaForm(forms.ModelForm):
     class Meta:
@@ -27,6 +26,16 @@ class IndicadorForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         for i in self.fields:
             self.fields[i].widget.attrs.update({'class':'form-control'})
+        
+        self.fields['tipo'].queryset = Tipo.objects.none()
+        if 'ambito' in self.data:
+            try:
+                ambito_id = int(self.data.get('ambito'))
+                self.fields['tipo'].queryset = Tipo.objects.filter(ambito_id=ambito_id).order_by('nombre')
+            except (ValueError, TypeError):
+                pass  # invalid input from the client; ignore and fallback to empty Tipo queryset
+        elif self.instance.pk:
+            self.fields['tipo'].queryset = self.instance.ambito.tipo_set.order_by('nombre')
 
 class MetricaFormEditar(forms.ModelForm):
     class Meta:
